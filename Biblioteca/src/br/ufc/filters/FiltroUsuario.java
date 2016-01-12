b937438a501a52866613066f2dbd779d02d7fa80
Esdras Lemos <esdras_lemos@hotmail.com>
2016-01-12 18:31:36 -0300

    Add FiltroUsuario

--- Biblioteca/src/br/ufc/filters/FiltroUsuario.java
+++ Biblioteca/src/br/ufc/filters/FiltroUsuario.java
@@ -1,6 +1,7 @@
 package br.ufc.filters;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,8 +10,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import br.ufc.model.dao.UsuarioDAO;
 import br.ufc.model.javabeans.Usuario;
 
 public class FiltroUsuario implements Filter{
@@ -19,15 +22,21 @@ public class FiltroUsuario implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc)
 			throws IOException, ServletException {
 
+		Connection connection = (Connection) request.getAttribute("connection");
+		
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpSession session = req.getSession(false);
 		
+		HttpServletResponse res = (HttpServletResponse) response;
+		
 		if(session != null) {
 			Usuario usuario = (Usuario) session.getAttribute("usuario");
-			String uri = req.getRequestURI();
-			
+			UsuarioDAO dao = new UsuarioDAO(connection);
+			if(dao.permission(usuario.getTipo(), req.getRequestURI()))
+				fc.doFilter(request, response);
+			else
+				res.sendRedirect("/index.jsp");
 		}
-		
 	}
 
 	@Override
