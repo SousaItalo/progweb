487af20ec91b37b4088f7b45f2131889a11a997a
Esdras Lemos <esdras_lemos@hotmail.com>
2016-01-12 21:55:26 -0300

    Fix pagina servlet

--- Biblioteca/src/br/ufc/controller/servlets/PaginaServlet.java
+++ Biblioteca/src/br/ufc/controller/servlets/PaginaServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class PaginaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -12,10 +13,14 @@ public class PaginaServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		String prefixo = "/WEB-INF/jsp/";
-		String pagina = prefixo + request.getParameter("pagina");
-		request.getRequestDispatcher(pagina).forward(request, response);
-		
+		HttpSession session = request.getSession(false);
+		if(session != null) {
+			String prefixo = "/WEB-INF/jsp/";
+			String pagina = prefixo + request.getParameter("pagina");
+			request.getRequestDispatcher(pagina).forward(request, response);
+		} else {
+			response.sendRedirect("index.jsp");
+		}
 	}
 
 }
