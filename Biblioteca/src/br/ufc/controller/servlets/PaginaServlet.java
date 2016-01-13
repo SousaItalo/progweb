9b6dc4fbf596a0d7bb60f46606dd5a808b028745
Esdras Lemos <esdras_lemos@hotmail.com>
2016-01-12 21:01:18 -0300

    Add PaginaServlet

---
+++ Biblioteca/src/br/ufc/controller/servlets/PaginaServlet.java
@@ -0,0 +1,20 @@
+package br.ufc.controller.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PaginaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String pagina = request.getParameter("pagina");
+		request.getRequestDispatcher(pagina).forward(request, response);
+		
+	}
+
+}
